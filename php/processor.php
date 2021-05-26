22d321950c82d74b5b65aa6514a130aca9ecbb6b
Carlos <perez.carlos_a@yahoo.com>
2021-05-26 16:28:03 -0700

    committing from new folder in MAMP

---
+++ php/processor.php
@@ -0,0 +1,20 @@
+<?php
+
+if(isset($_POST['submit'])) {
+
+    $answer = $_POST['units'];
+
+    $value = $_POST['value'];
+    
+    if($answer == 'celsius') {
+        $c = ($value - 32) * 5/9;
+
+        echo $c;
+    } else if ($answer == 'fahrenheit') {
+        $f = ($value * 9) / 5 + 23;
+
+        echo $f;
+    }
+};
+
+?>
